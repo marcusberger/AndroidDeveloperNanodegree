5eac7678c5417e039c4c53d7bb1582fa07c7e111
Marcus Berger <marcusvcberger@gmail.com>
2016-10-24 01:03:48 -0200

    add toast in buttons

--- MyAppPortfolio/app/src/main/java/com/marcusberger/myportfolioapp/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/marcusberger/myportfolioapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.marcusberger.myportfolioapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button btnMessage = (Button) findViewById(R.id.Btn1);
+        btnMessage.setOnClickListener(btn1);
+
+        Button btnMessage2 = (Button) findViewById(R.id.Btn2);
+        btnMessage2.setOnClickListener(btn2);
+
+        Button btnMessage3 = (Button) findViewById(R.id.Btn3);
+        btnMessage3.setOnClickListener(btn3);
+
+        Button btnMessage4 = (Button) findViewById(R.id.Btn4);
+        btnMessage4.setOnClickListener(btn4);
+
+        Button btnMessage5 = (Button) findViewById(R.id.Btn5);
+        btnMessage5.setOnClickListener(btn5);
+
+        Button btnMessage6 = (Button) findViewById(R.id.Btn6);
+        btnMessage6.setOnClickListener(btn6);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,27 +48,59 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+        private View.OnClickListener btn1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Toast.makeText(MainActivity.this, "This button will launch my popular movie app",
+                       Toast.LENGTH_LONG).show();
 
-        return super.onOptionsItemSelected(item);
-    }
+            }
+        };
+
+    private View.OnClickListener btn2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "This button will launch my stock hawk app",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    private View.OnClickListener btn3 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "This button will launch my build it bigger app",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    private View.OnClickListener btn4 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "This button will launch my make your app material app",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    private View.OnClickListener btn5 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "This button will launch my go ubiquitous app",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    private View.OnClickListener btn6 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "This button will launch my capstone app",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
